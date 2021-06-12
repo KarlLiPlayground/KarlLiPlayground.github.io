83047f478098e9c1e00d60cb37d8594ab101b1d4
KarlDev <kelvinliudev@outlook.com>
2021-06-12 12:44:31 +0900

    uuu

--- kgg1/js/default.thm_5b7723b6.js
+++ kgg1/js/default.thm_5b7723b6.js
@@ -14,11 +14,11 @@ window.skins=window.skins||{};
 	__extends(ButtonSkin, _super);
 	function ButtonSkin() {
 		_super.call(this);
-		this.skinParts = ["labelDisplay","iconDisplay"];
+		this.skinParts = ["iconDisplay"];
 		
 		this.minHeight = 50;
 		this.minWidth = 100;
-		this.elementsContent = [this._Image1_i(),this.labelDisplay_i(),this.iconDisplay_i()];
+		this.elementsContent = [this.iconDisplay_i()];
 		this.states = [
 			new eui.State ("up",
 				[
@@ -26,39 +26,16 @@ window.skins=window.skins||{};
 			,
 			new eui.State ("down",
 				[
-					new eui.SetProperty("_Image1","source","button_down_png")
+					new eui.SetProperty("iconDisplay","alpha",0.5)
 				])
 			,
 			new eui.State ("disabled",
 				[
-					new eui.SetProperty("_Image1","alpha",0.5)
 				])
 		];
 	}
 	var _proto = ButtonSkin.prototype;
 
-	_proto._Image1_i = function () {
-		var t = new eui.Image();
-		this._Image1 = t;
-		t.percentHeight = 100;
-		t.scale9Grid = new egret.Rectangle(1,3,8,8);
-		t.source = "button_up_png";
-		t.percentWidth = 100;
-		return t;
-	};
-	_proto.labelDisplay_i = function () {
-		var t = new eui.Label();
-		this.labelDisplay = t;
-		t.bottom = 8;
-		t.left = 8;
-		t.right = 8;
-		t.size = 20;
-		t.textAlign = "center";
-		t.textColor = 0xFFFFFF;
-		t.top = 8;
-		t.verticalAlign = "middle";
-		return t;
-	};
 	_proto.iconDisplay_i = function () {
 		var t = new eui.Image();
 		this.iconDisplay = t;
